858e57ef822ee6938dca27047ad524b7eabf271a
Ryosuke YASUOKA <yasuoka0830@gmail.com>
2019-02-01 01:06:27 +0900

    update

---
+++ functions/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello, world!");
+
+    another_function(5, 6);
+}
+
+fn another_function(x: u32, y: i32) {
+    println!("Another function.");
+    println!("The value of x is : {}", x);
+    println!("The value of y is : {}", y);
+}
