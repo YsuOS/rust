dbc5a0fb2b552cc431b9b3bb0b683a9bbcd9d32a
yasuoka ryosuke <yasuoka0830@gmail.com>
2019-02-02 17:26:35 +0900

    add functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,11 +1,32 @@
 fn main() {
+    // Hello, world!
     println!("Hello, world!");
 
     another_function(5, 6);
+
+    let x = 8;
+    let y = {
+        let x = 3;
+        println!("The value of x is : {}", x);
+        plus_one(x)
+    };
+
+    another_function(x, y);
+    let x : i32 = five();
+    println!("The value of x is : {}", x);
+
 }
 
 fn another_function(x: u32, y: i32) {
     println!("Another function.");
     println!("The value of x is : {}", x);
     println!("The value of y is : {}", y);
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
