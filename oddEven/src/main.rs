1e274a6a0ad8e20087ddff218ba76e450f6808f9
yasuoka ryosuke <yasuoka0830@gmail.com>
2019-02-03 19:43:56 +0900

    updated oddEven

--- oddEven/src/main.rs
+++ oddEven/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let a = 3;
+    let b = 4;
+   
+    let ans : str = odd_even(a * b);
+    println!("answer is {}", ans);
+}
+
+fn odd_even(x : u32) -> str {
+    if x % 2 == 0 {
+        "Even"
+    } else {
+        "Odd"
+    }
 }
