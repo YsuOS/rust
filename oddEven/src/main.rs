8f2832ef162f473afcb12b977ee4c8b6024c17ca
Ryosuke YASUOKA <yasuoka0830@gmail.com>
2019-02-07 17:53:05 +0900

    add ownership

--- oddEven/src/main.rs
+++ oddEven/src/main.rs
@@ -1,12 +1,12 @@
 fn main() {
     let a = 3;
     let b = 4;
-   
-    let ans : str = odd_even(a * b);
+
+    let mut ans: String = odd_even(a * b);
     println!("answer is {}", ans);
 }
 
-fn odd_even(x : u32) -> str {
+fn odd_even(x : u32) -> mut String {
     if x % 2 == 0 {
         "Even"
     } else {
