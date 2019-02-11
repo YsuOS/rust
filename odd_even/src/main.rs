9ba571fc2777884906b8e814b345929fa6c9c507
yasuoka ryosuke <yasuoka0830@gmail.com>
2019-02-11 13:56:20 +0900

    finish odd_even

--- odd_even/src/main.rs
+++ odd_even/src/main.rs
@@ -1,19 +1,25 @@
-use std::env;
-
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let s = {
+        let mut s = String::new();
+        std::io::stdin().read_line(&mut s).unwrap();
+        s.trim_right().to_owned()
+    };
+    let (a, b) = {
+        let mut ws = s.split_whitespace();
+        let a: i32 = ws.next().unwrap().parse().unwrap();
+        let b: i32 = ws.next().unwrap().parse().unwrap();
+        (a, b)
+    };
 //    let a = 3;
 //    let b = 4;
 
-    let a: u32 = args[1].parse().unwrap();
-    let b: u32 = args[2].parse().unwrap();
 
     let ans = odd_even(&(a * b));
 //    println!("answer is {}", ans);
     println!("{}", ans);
 }
 
-fn odd_even(x: &u32) -> String {
+fn odd_even(x: &i32) -> String {
     let ans;
     if x % 2 == 0 {
         ans = String::from("Even");
