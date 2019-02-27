b8bb2705eca35d83e3e2c524d40caa5448046338
yasuoka ryosuke <yasuoka0830@gmail.com>
2019-02-28 00:54:48 +0900

    updated enums

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -46,42 +46,42 @@ fn value_in_cents(coin: Coin) -> u32 {
     }
 }
 
-//fn plus_one(x: Option<i32>) -> Option<i32> {
-//    match x {
-//        None => None,
-//        Some(i) => Some(i + 1),
-//    }
-//}
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
 
 fn main() {
     let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
+//    let six = IpAddrKind::V6;
     println!("{:?}", four);
-    println!("{:?}", six);
+//    println!("{:?}", six);
 
     let msg_mv = Message::Move { x: 3, y: 2 };
     println!("{:?}", msg_mv);
 
-//    let some_number = Some(5);
-//    let some_string = Some("a string");
+    let some_number = Some(5);
+    let some_string = Some("a string");
 
-//    let absent_number: Option<i32> = None;
+    let absent_number: Option<i32> = None;
 
     let x: i8 = 5;
 //    let y: Option<i8> = Some(5);
 
 //    let sum = x + y;
 
-//    let five = Some(5);
-//    let six = plus_one(five);
-//    let none = plus_one(None);
-//    println!("{:?}", six);
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    println!("{:?}", six);
 
     let quarter = value_in_cents(Coin::Quarter(UsState::Alabama));
     println!("{}", quarter);
 
 //    let some_u8_value = 0u8;
-    let some_u8_value = 3;
+//    let some_u8_value = 3;
 //    match some_u8_value {
 //        1 => println!("one"),
 //        2 => println!("two"),
@@ -89,6 +89,7 @@ fn main() {
 //        _ => (),
 //    }
     
+    let some_u8_value = Some(3);
     if let Some(3) = some_u8_value {
         println!("three");
     }
