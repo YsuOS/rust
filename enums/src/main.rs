fd45c9249dd227ebb9d6a21d01394336bc153cb5
yasuoka ryosuke <yasuoka0830@gmail.com>
2019-02-28 00:45:28 +0900

    tmp update enums

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,9 +1,10 @@
-#[derive(Debug)]
+#[derive(Debug)] // this is attribute
 
-//enum IpAddrKind {
-//    V4,
-//    V6,
-//}
+enum IpAddrKind {
+    V4,
+    V6,
+}
+#[derive(Debug)]
 
 enum Message {
     Quit,
@@ -16,63 +17,81 @@ enum Message {
 //    Some(T),
 //    None,
 //}
-//
-//enum Coin {
-//    Penny,
-//    Nickel,
-//    Dime,
-//    Quarter(UsState),
-//}
-//
-//enum UsState {
-//    Alabama,
-//    Alaska,
-//}
-//
-//fn value_in_cents(coin: Coin) -> u32 {
-//    match coin {
-//        Coin::Penny => {
-//            println!("Lucky penny!");
-//            1
-//        },
-//        Coin::Nickel => 5,
-//        Coin::Dime => 10,
-//        Coin::Quarter(state) => {
-//            println!("State quarter from {:?}!", state);
-//            25
-//        }
-//    }
-//}
-//
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
 //fn plus_one(x: Option<i32>) -> Option<i32> {
 //    match x {
 //        None => None,
 //        Some(i) => Some(i + 1),
 //    }
 //}
-fn main() {
-//    let four = IpAddrKind::V4;
-//    let six = IpAddrKind::V6;
-//    println!("{:?}", four);
-//    println!("{:?}", six);
 
-// FIXME
-    let mv = Message::Move { x: 3, y: 2 };
-    println!("{:?}", mv);
-//
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+    println!("{:?}", four);
+    println!("{:?}", six);
+
+    let msg_mv = Message::Move { x: 3, y: 2 };
+    println!("{:?}", msg_mv);
+
 //    let some_number = Some(5);
 //    let some_string = Some("a string");
-//
-////    let absent_number: Option<i32> = None;
-//
-//    let x: i8 = 5;
+
+//    let absent_number: Option<i32> = None;
+
+    let x: i8 = 5;
 //    let y: Option<i8> = Some(5);
-//
-////    let sum = x + y;
-//
+
+//    let sum = x + y;
+
 //    let five = Some(5);
 //    let six = plus_one(five);
 //    let none = plus_one(None);
+//    println!("{:?}", six);
+
+    let quarter = value_in_cents(Coin::Quarter(UsState::Alabama));
+    println!("{}", quarter);
+
+//    let some_u8_value = 0u8;
+    let some_u8_value = 3;
+//    match some_u8_value {
+//        1 => println!("one"),
+//        2 => println!("two"),
+//        3 => println!("three"),
+//        _ => (),
+//    }
+    
+    if let Some(3) = some_u8_value {
+        println!("three");
+    }
 }
 
 
