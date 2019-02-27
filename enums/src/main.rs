14d99aaee88158366ebd648f285310ec86948bbe
yasuoka ryosuke <yasuoka0830@gmail.com>
2019-02-27 23:29:55 +0900

    tmp update enums

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -12,11 +12,67 @@ enum Message {
     ChangeColor(i32, i32, i32),
 }
 
+enum Option<T> {
+    Some(T),
+    None,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
 fn main() {
     let four = IpAddrKind::V4;
     let six = IpAddrKind::V6;
-    let msg_mv = Message::Move { x: 2, y: 3 };
     println!("{:?}", four);
     println!("{:?}", six);
-    println!("{:?}", msg_mv);
+
+// FIXME
+    let mv = Message::Move{ x: 3, y: 2 };
+    println!("{:?}", mv);
+
+    let some_number = Some(5);
+    let some_string = Some("a string");
+
+//    let absent_number: Option<i32> = None;
+
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
+
+//    let sum = x + y;
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
 }
+
+
