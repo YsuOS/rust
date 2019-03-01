91693fcc2f4db9dcd00bfcd6157c2a606e963da5
Ryosuke YASUOKA <yasuoka0830@gmail.com>
2019-03-01 19:26:42 +0900

    finish mods

--- mods/communicator/src/lib.rs
+++ mods/communicator/src/lib.rs
@@ -1,3 +1,37 @@
 pub mod client;
 
 pub mod network;
+
+mod outermost { // add pub, there is no meaning
+    pub fn middle_function() {}
+
+    fn middle_secret_function() {} //add pub
+
+    mod inside { //add pub
+        pub fn inner_function() {
+            ::outermost::middle_secret_function(); 
+            // can call module which is the parent module
+        }
+
+        fn secret_function() {}
+    }
+}
+
+fn try_me() {
+//    outermost::middle_function();
+//    outermost::middle_secret_function();
+//    outermost::inside::inner_function();
+//    outermost::inside::secret_function();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::client;
+
+    #[test]
+    fn it_works() {
+//        assert_eq!(2 + 2, 4);
+        client::connect();
+//        ::client::connect();
+    }
+}
--- mods/communicator/src/main.rs
+++ mods/communicator/src/main.rs
@@ -1,5 +1,32 @@
 extern crate communicator;
 
+mod a {
+    pub mod series {
+        pub mod of {
+            pub fn nested_modules() {}
+        }
+    }
+}
+
+use a::series::of::nested_modules;
+use a::series::of;
+
+enum TrafficLight {
+    Red,
+    Yellow,
+    Green,
+}
+
+use TrafficLight::{Red, Yellow};
+use TrafficLight::*;
+
 fn main() {
     communicator::client::connect();
+    of::nested_modules();
+    nested_modules();
+
+    let red = Red;
+    let yellow = Yellow;
+//    let green = TrafficLight::Green;
+    let green = Green;
 }
