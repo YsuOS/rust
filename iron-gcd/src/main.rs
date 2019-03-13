bd5a2bd58978d77ae9b92c6e135f0a3719586f80
yasuoka ryosuke <yasuoka0830@gmail.com>
2019-03-14 07:45:23 +0900

    add debug

--- iron-gcd/src/main.rs
+++ iron-gcd/src/main.rs
@@ -19,6 +19,7 @@ fn main() {
 fn get_form(_request: &mut Request) -> IronResult<Response> {
     let mut response = Response::new();
 
+    println!("[debug] get_form Request {:?}", _request);
     response.set_mut(status::Ok);
     response.set_mut(mime!(Text/Html; Charset=Utf8));
     response.set_mut(r#"
@@ -41,6 +42,7 @@ use urlencoded::UrlEncodedBody;
 fn post_gcd(request: &mut Request) -> IronResult<Response> {
     let mut response = Response::new();
 
+    println!("[debug] post_gcd Request {:?}", request);
     let form_data = match request.get_ref::<UrlEncodedBody>() {
         Err(e) => {
             response.set_mut(status::BadRequest);
@@ -49,6 +51,7 @@ fn post_gcd(request: &mut Request) -> IronResult<Response> {
         }
         Ok(map) => map
     };
+    println!("[debug] data_form {:?}", form_data);
 
     let unparsed_numbers = match form_data.get("n") {
         None => {
@@ -58,6 +61,7 @@ fn post_gcd(request: &mut Request) -> IronResult<Response> {
         }
         Some(nums) => nums
     };
+    println!("[debug] unparsed_numbers {:?}", unparsed_numbers);
 
     let mut numbers = Vec::new();
     for unparsed in unparsed_numbers {
@@ -72,6 +76,7 @@ fn post_gcd(request: &mut Request) -> IronResult<Response> {
             Ok(n) => { numbers.push(n); }
         }
     }
+    println!("[debug] numbers {:?}", numbers);
 
     let mut d = numbers[0];
     for m in &numbers[1..] {
@@ -83,6 +88,7 @@ fn post_gcd(request: &mut Request) -> IronResult<Response> {
     response.set_mut(
         format!("The greatest common divisor of the numbers {:?} is <b>{}</b>\n",
                 numbers, d));
+    println!("[debug] post_gcd Request {:?}", response);
     Ok(response)
 }
 
