a65cb54bf014631c73299886b19640098592ff75
yasuoka ryosuke <yasuoka0830@gmail.com>
2019-04-10 00:38:53 +0900

    tmp

---
+++ card_game_for_two/src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    let mut n = String::new();
+    std::io::stdin().read_line(&mut n).unwrap();
+    let n: u32 = n.trim().parse().unwrap();
+    let mut a = String::new();
+    std::io::stdin().read_line(&mut a).unwrap();
+    let mut v: Vec<u32> = a.trim().split_whitespace()
+        .map(|x| x.parse().unwrap()).collect();
+    v.sort();
+
+    let mut alice = 0;
+    let mut bob = 0;
+    let mut tmp = 0;
+    for x in v {
+        if (tmp % 2 == 0) {
+            alice += x;
+        }
+        if (tmp % 2 == 1) {
+            bob += x;
+        }
+        tmp += 1;
+    }
+    let ans = alice - bob;
+    println!("{}", ans);
+}
