2fa21d51342194924a388fa8f33b09337f150532
yasuoka ryosuke <yasuoka0830@gmail.com>
2019-04-10 14:03:09 +0900

    update

--- otoshidama/src/main.rs
+++ otoshidama/src/main.rs
@@ -4,9 +4,9 @@ fn main() {
     let v: Vec<u32> = ny.trim().split_whitespace()
         .map(|x| x.parse().unwrap()).collect();
 
-    for i in 0..v[0] {
-        for j in 0..v[0]-i {
-            if (10000 * i + 5000 * j + 1000 * v[0]-i-j == v[1]) {
+    for i in 0..v[0]+1 {
+        for j in 0..v[0]+1-i {
+            if (10000 * i + 5000 * j + 1000 * (v[0]-i-j) == v[1]) {
                 println!("{} {} {}", i, j, v[0]-i-j);
                 return;
             }
