a83dcaf718048af3dfe98c3840f53d5c6aeee761
Ryosuke YASUOKA <yasuoka0830@gmail.com>
2019-04-19 23:19:02 +0900

    update

--- placing_marbles/src/main.rs
+++ placing_marbles/src/main.rs
@@ -1,19 +1,17 @@
 fn main() {
 
-//    let s = String::from("101");
     let mut s = String::new();
     std::io::stdin().read_line(&mut s).unwrap();
 
-//    println!("{}",s);
-    let mut ans = 0;
-    if &s[0..1] == "1" {
-        ans = ans + 1;
-    }
-    if &s[1..2] == "1" {
-        ans = ans + 1;
-    }
-    if &s[2..3] == "1" {
-        ans = ans + 1;
-    }
-    println!("{}", ans);
+//    let mut ans = 0;
+//    if &s[0..1] == "1" {
+//        ans = ans + 1;
+//    }
+//    if &s[1..2] == "1" {
+//        ans = ans + 1;
+//    }
+//    if &s[2..3] == "1" {
+//        ans = ans + 1;
+//    }
+//    println!("{}", ans);
 }
